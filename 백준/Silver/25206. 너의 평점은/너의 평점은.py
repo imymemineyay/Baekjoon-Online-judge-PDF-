eed0d3c4f8a4c5a913713a4b7b3e8a888ea16f4f
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2023-02-28 13:14:37 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,43 +1,17 @@
+grades = {'A+': 4.5 , 'A0': 4.0, 'B+':3.5, 'B0' :3.0, 'C+':2.5, 
+         'C0':2.0, 'D+':1.5, 'D0':1.0, 'F': 0.0, 'P': 0.0}
 score_X_gpa = [] # 학점 * 과목 평점의 합을 구하기 위한 리스트
 sum_scores = [] # 학점 총합을 구하기 위한 리스트
 
 for _ in range(20): # 20 줄
     class_name, score, grade = input().split() # 과목, 학점, 등급
-    score = float(score) # 학점 소수점화 
-
-    ## 등급별 과목평점 지정
-    if grade == 'A+':
-        gpa = 4.5 * score
-        num = score
-    elif grade == 'A0':
-        gpa = 4.0 * score
-        num = score
-    elif grade == 'B+':
-        gpa = 3.5 * score
-        num = score
-    elif grade == 'B0':
-        gpa = 3.0 * score
-        num = score
-    elif grade == 'C+':
-        gpa = 2.5 * score
-        num = score
-    elif grade == 'C0':
-        gpa = 2.0 * score
-        num = score
-    elif grade == 'D+':
-        gpa = 1.5 * score
-        num = score
-    elif grade == 'D0':
-        gpa = 1.0 * score
-        num = score
-    elif grade == 'F':
-        gpa = 0.0 * score
-        num = score
-    elif grade == 'P': 
-        gpa = 0.0
-        num = 0.0
-    score_X_gpa.append(gpa)
-    sum_scores.append(num)
+    
+    if grade == 'P':
+        score = 0.0
+    else: 
+        score = float(score) # 학점 소수점화 
+    score_X_gpa.append(score*grades[grade])
+    sum_scores.append(score)
 
 GPA = sum(score_X_gpa) / sum(sum_scores) 
-print('%.6f'%GPA)
+print('%.6f'%GPA) # 소수점 이하 6자리
