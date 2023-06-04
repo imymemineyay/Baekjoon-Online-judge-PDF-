19b40fb2b88dc036595bb347b146e3c341a1c5c8
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2023-06-04 14:27:20 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 696 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,9 @@
+a,b,c,d,e,f = map(int,input().split())
+
+for i in range(-999,1000):
+    for j in range(-999,1000):
+        if a*i + b*j == c:
+            if d*i + e*j == f:
+                result = [i,j]
+                break
+print(*result)
