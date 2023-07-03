95bf413f2a6cff64a5d741ee1932d99f2ea8fba1
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2023-07-03 15:53:33 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,10 +1,13 @@
 N = int(input())
-num = 2*N-1
-for j in range(1,2*N,2):
-    star = '*' * j
-    center_star1 = star.center(num)
-    print(center_star1.rstrip())
-for k in range(2*N-3,0,-2):
-    star = '*' * k
-    center_star2 = star.center(num)
-    print(center_star2.rstrip())
+
+# 두 가지를 다르게 구해줘서 합치기 
+# 1 방법 : 앞의 공백 (4,3,2,1,0), 별 개수 (1,3,5,7,9)
+for i in range(2*N//2,0,-1):
+    blank = i-1 
+    star = 2*N-2*i+1
+    print(' '*blank,'*'*star,sep='')
+# 2 방법 : 앞의 공백 (1,2,3,4), 별 개수 (7,5,3,1)
+for k in range(1,2*N//2):
+    blank = k
+    star = 2*N - (2*k+1) 
+    print(' '*blank, '*'*star, sep='')
