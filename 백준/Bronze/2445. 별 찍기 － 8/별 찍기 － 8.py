eaf0fb9bb8b5a70cf2ef8a5dfd024d9ea4222a67
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2023-07-08 09:38:45 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -1,9 +1,8 @@
-N = int(input())
+# 절대값을 사용하여 풀는 법 
+n = int(input())
 
-for i in range(1,2*N):
-    if i <= (2*N//2):
-        print('*'*i,' '*(2*N-2*i),'*'*i,sep='')
-    else:
-        star = 2*N - i
-        blank = 2*N- (2*N - i)*2
-        print('*'*star,' '*blank,'*'*star,sep='')
+for i in range(1, 2*n):
+    col = 2 *n 
+    star = (n - abs(n-i))
+    l = '*' *star + ' '*(col-2*star)+ "*" * star
+    print(l)
