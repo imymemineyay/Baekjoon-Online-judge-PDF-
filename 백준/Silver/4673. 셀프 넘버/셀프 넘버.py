3e1535900dd44c262b3155d5f0e3198361e25593
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2023-07-25 23:17:56 +0900

    [Silver V] Title: 셀프 넘버, Time: 168 ms, Memory: 115384 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,9 +1,14 @@
-generator_lst = []
-i = 1
-while i <= 10000:
-    generator = i + (i // 1000) + (i % 1000 // 100) + (i % 1000 % 100 // 10) + (i % 1000 % 100 % 10)
-    generator_lst.append(generator)
-    i += 1
-for k in range(1,10001):
-    if k not in generator_lst:
-        print(k)      
+n = 0
+lst = []
+
+dns = []
+for i in range(1, 10000):
+
+  # make d(i)
+
+  dn = i + sum(list(map(int, list(str(i)))))
+  dns.append(dn)
+
+for i in range(1, 10000):
+  if i not in dns:
+    print(i)
