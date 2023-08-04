0545c5db241401ded36d13a0b09e8b849c274631
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2023-08-04 10:35:41 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 115400 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,14 +1,19 @@
 N = int(input())
 
-lst = [int(input()) for _ in range(N)]
+# 리스트 안에 수 넣기
+num_list = [int(input()) for _ in range(N)]
+sorted_list = [0 for _ in range(N)]
 
+# 삽입 정렬
+# 카드 하나를 뽑고 왼쪽으로 옮기기 
 
-for i in range(N-1):
-    min_idx = i
-    for j in range(i+1,N):
-        if lst[min_idx] > lst[j]:
-            min_idx = j
-    lst[i], lst[min_idx] = lst[min_idx], lst[i]
+for i in range(N):
+  for k in range(N):
+    if num_list[i] > num_list[k]:
+        a = num_list[i]
+        b = num_list[k]
+        num_list[k] = a
+        num_list[i] = b
 
-for k in lst:
-    print(k)
+for i in reversed(num_list):
+    print(i)
