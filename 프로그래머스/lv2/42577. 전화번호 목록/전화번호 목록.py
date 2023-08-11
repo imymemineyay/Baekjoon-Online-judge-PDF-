d32dfed9fa9e987bc8d7066b0c754fe1b4e18617
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2023-08-11 18:42:48 +0900

    [level 2] Title: 전화번호 목록, Time: 449.00 ms, Memory: 34.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    answer = True
+    phone_nums = {num:1 for num in phone_book}
+    
+    for p in phone_book:
+        tmp = ''
+        for k in p:
+            tmp += k
+            if tmp in phone_nums and tmp != p:
+                answer = False
+    return answer       
