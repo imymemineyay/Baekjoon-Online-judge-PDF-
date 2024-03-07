e29d1826f051e8f436baad54031b4e6638b0628f
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-07 22:39:34 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,5 @@
+SELECT member_id, member_name, gender, 
+       DATE_FORMAT(date_of_birth,'%Y-%m-%d') AS date_of_birth
+FROM member_profile
+WHERE MONTH(date_of_birth) = 3 AND gender = 'w' AND tlno IS NOT NULL
+ORDER BY member_id; 
