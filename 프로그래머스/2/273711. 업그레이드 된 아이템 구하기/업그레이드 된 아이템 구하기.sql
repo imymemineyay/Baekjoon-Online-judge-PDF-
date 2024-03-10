e6fffeb95cc5de08761719905f089e24c0c1f28d
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-10 19:32:19 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,4 +1,11 @@
+-- #2 
 SELECT item_id, item_name, rarity
-    FROM  item_info JOIN item_tree USING(item_id)
-    WHERE parent_item_id in (SELECT item_id FROM item_info WHERE rarity ='rare')
-    ORDER BY item_id DESC;
+    FROM item_info 
+    WHERE item_id IN (SELECT item_id 
+                      FROM item_tree 
+                      WHERE parent_item_id IS NOT NULL 
+                            AND
+                            parent_item_id IN (SELECT item_id
+                                                FROM item_info
+                                                WHERE rarity = 'rare'))
+    ORDER BY item_id DESC; 
