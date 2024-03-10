48ae31c5e140a8d7972e9748ed3568cdbd711d25
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-10 20:53:45 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,4 +1,6 @@
-SELECT ITEM_ID, ITEM_NAME
-    FROM ITEM_INFO
-    WHERE ITEM_ID IN (SELECT ITEM_ID FROM ITEM_TREE WHERE PARENT_ITEM_ID IS NULL)
-    ORDER BY ITEM_ID
+-- #2
+
+SELECT item_id, item_name 
+    FROM item_info
+    WHERE item_id IN (SELECT item_id FROM item_tree WHERE parent_item_id IS NULL)
+    ORDER BY item_id;
