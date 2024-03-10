401e3ac6a5cbafea9133f0b5b346cebc5425d1c6
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-10 21:15:21 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -1,4 +1,8 @@
-SELECT ITEM_ID, ITEM_NAME, RARITY
-    FROM ITEM_INFO
-    WHERE ITEM_ID NOT IN (SELECT PARENT_ITEM_ID FROM ITEM_TREE WHERE PARENT_ITEM_ID IS NOT NULL )
-    ORDER BY ITEM_ID DESC;
+-- #2
+
+SELECT item_id, item_name, rarity
+    FROM item_info
+    WHERE item_id NOT IN (SELECT parent_item_id 
+                          FROM item_tree 
+                          WHERE parent_item_id IS NOT NULL)
+    ORDER BY item_id DESC;
