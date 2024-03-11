5a761deaeb5cc3f01f3730e13c31fd35c1a22660
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-12 04:12:01 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,7 +1,14 @@
--- #2
+-- #2 
+
+-- first_half의 pk 는 flavor, fk는 shipment_id
+-- july의 pk는 shipment_id, fk는 flavor
+
+-- first_half 1 : july 다 (flavor을 조회하는 문제이기 때문)
 
 SELECT flavor
-    FROM july AS j LEFT OUTER JOIN first_half AS h USING(flavor)
-    GROUP BY h.flavor
-    ORDER BY AVG(h.total_order) + SUM(j.total_order) DESC 
-    LIMIT 3;
+FROM first_half AS h JOIN july AS j USING(flavor)
+-- first_half 의 총 주문량은 july와 조인되면서 중복되게 됨 
+-- 따라서 avg를 해서 중복 제거값으로 구해줘야함
+GROUP BY flavor
+ORDER BY AVG(h.total_order) + SUM(j.total_order) DESC
+LIMIT 3;
