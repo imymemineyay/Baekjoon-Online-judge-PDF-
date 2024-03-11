aca424902e3deb1480cc2fa095e376d4c43042f8
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-11 23:38:29 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/README.md
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 주문량이 많은 아이스크림들 조회하기 - 133027 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133027#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133027) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 2월 5일 17:18:6
+2024년 03월 11일 23:38:27
 
 ### 문제 설명
 
--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,12 +1,7 @@
-WITH july_tot AS (
-    SELECT flavor, SUM(total_order) AS total_order
-    FROM july
-    GROUP BY flavor
-)
+-- #2
 
-SELECT f.flavor
-FROM first_half AS f
-JOIN july_tot AS j ON f.flavor = j.flavor
-GROUP BY f.flavor
-ORDER BY SUM(f.total_order + j.total_order) DESC
-LIMIT 3;
+SELECT flavor
+    FROM july AS j LEFT OUTER JOIN first_half AS h USING(flavor)
+    GROUP BY h.flavor
+    ORDER BY AVG(h.total_order) + SUM(j.total_order) DESC 
+    LIMIT 3;
