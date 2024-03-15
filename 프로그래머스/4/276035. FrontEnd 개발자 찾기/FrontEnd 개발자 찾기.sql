7a55e2dcb01eb7af421acbb97ce5f41b9d2d9732
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-15 10:52:00 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,4 +1,6 @@
-SELECT DISTINCT(id), email, first_name, last_name
-    FROM skillcodes, developers
-    WHERE (skill_code & code) AND category = "Front End"
-    ORDER BY id;
+-- # 2
+
+SELECT DISTINCT id, email, first_name, last_name
+FROM skillcodes JOIN developers
+WHERE (skill_code & code) AND category = 'Front End'
+ORDER BY id;
