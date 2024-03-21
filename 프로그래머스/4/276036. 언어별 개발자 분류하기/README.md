f9fd3ef2ebb33c93841bfd14196e07d4277b8dc4
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-21 13:21:33 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/README.md
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 언어별 개발자 분류하기 - 276036 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/276036#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/276036) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 05일 21:37:55
+2024년 03월 21일 13:21:31
 
 ### 문제 설명
 
--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,16 +1,20 @@
-WITH front AS (SELECT SUM(code) AS tot
-                FROM skillcodes
-                WHERE category = 'Front End')
+-- #2 
 
-SELECT CASE
-          WHEN (skill_code&(SELECT tot FROM front)) AND (skill_code&(SELECT code FROM skillcodes WHERE name ='Python')) 
-          THEN 'A'
-          WHEN skill_code&(SELECT code FROM skillcodes WHERE name ='C#') 
-          THEN 'B'
-          WHEN (skill_code&(SELECT tot FROM front)) 
-          THEN 'C'    
-       END AS grade,id,email
-    FROM developers 
-    GROUP BY id, email,skill_code
-    HAVING grade IS NOT NULL
-    ORDER BY grade, id;
+WITH grade_info AS (
+    SELECT DISTINCT 
+           CASE
+              WHEN skill_code & (SELECT SUM(code) FROM skillcodes
+                                 GROUP BY category HAVING category = 'Front End' )  
+                    AND skill_code & (SELECT code 
+                                      FROM skillcodes 
+                                      WHERE name = 'Python') THEN 'A'
+              WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#')  THEN 'B'
+              WHEN skill_code & (SELECT SUM(code) FROM skillcodes
+                                 GROUP BY category HAVING category = 'Front End' ) THEN 'C'
+           END AS grade, id, email
+    FROM developers FULL JOIN skillcodes
+    WHERE code & skill_code
+    ORDER BY id
+)
+
+SELECT grade, id, email FROM grade_info WHERE grade IS NOT NULL ORDER BY grade,id;
