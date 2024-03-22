c274fffac6a99a825148413ef948845973c0e690
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-22 11:34:37 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,19 +1,18 @@
-SELECT e.emp_no, 
-       e.emp_name, 
-       CASE 
-         WHEN AVG(g.score) >= 96 THEN 'S'
-         WHEN AVG(g.score) >= 90 THEN 'A'
-         WHEN AVG(g.score) >= 80 THEN 'B'
-         ELSE 'C'
-       END AS grade, 
-       CASE 
-         WHEN AVG(g.score) >= 96 THEN e.sal*0.2
-         WHEN AVG(g.score) >= 90 THEN e.sal*0.15
-         WHEN AVG(g.score) >= 80 THEN e.sal*0.1
-         ELSE e.sal*0
-       END AS bonus
-    FROM hr_employees AS e
-        JOIN hr_grade AS g
-        ON e.emp_no = g.emp_no
-    GROUP BY e.emp_no
-    ORDER BY e.emp_no;
+-- #2
+
+SELECT emp_no, emp_name, 
+        CASE 
+            WHEN AVG(score)>=96 THEN 'S'
+            WHEN AVG(score)>=90 THEN 'A'
+            WHEN AVG(score)>=80 THEN 'B'
+            ELSE 'C'
+        END AS grade,
+        CASE 
+            WHEN AVG(score)>=96 THEN sal*0.2
+            WHEN AVG(score)>=90 THEN sal*0.15
+            WHEN AVG(score)>=80 THEN sal*0.1
+            ELSE 0
+        END AS bonus
+FROM hr_employees JOIN hr_grade USING (emp_no)
+GROUP BY emp_no
+ORDER BY emp_no;
