4e1a532313b83a6041b28ac292eeb09f83a08e27
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-23 16:45:54 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,23 @@
+# N마리 폰켓몬 중 N/2 ok
+# 종류에 따라 번호 부여
+# 최대한 다양한 종류의 폰켓몬 가지길 원하기 때문에 많은 종류의 폰켓몬을 포함해 N/2마리 선택
+# 폰켄못 종류번호가 담긴 배열 = nums
+# 폰켓몬 종류 개수의 최댓값 하나만 return
+
+
+# 포켓몬 종류는 여러개고 그 안에 겹치는 애도 있다.
+# 그런데 내가 가지고 갈 수 있는 최대의 개수는 N/2 이다. 
+# 그치만 N/2에는 겹치는 애들이 있을 수 있다. 
+# 그래서 종류가 몇갠지 그것이 중요하다 왜냐 안겹치니까~!
+
+def solution(nums): 
+    max_products = len(nums)//2 #가지고 갈수있는 개수
+    pocket_type = set(nums)# 포켓몬 종류 수 
+    num_pocket_type = len(pocket_type) #포켓몬 종류 개수
+    return min(max_products,num_pocket_type)   
+    
+    
+    
+
+
+    
