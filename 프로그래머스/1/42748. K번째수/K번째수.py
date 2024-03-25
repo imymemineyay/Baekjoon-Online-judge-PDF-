7d0e24a4ce5c888b2bc042ef920cf58f4ea86b50
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-25 14:10:12 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,6 @@
 def solution(array, commands):
     answer = []
-    for command in commands:
-        lst = array[command[0]-1:command[1]]
-        lst = sorted(lst)
-        answer.append(lst[command[2]-1])
+    for i,j,k in commands:
+        answer.append(sorted(array[i-1:j])[k-1])
+    
     return answer
