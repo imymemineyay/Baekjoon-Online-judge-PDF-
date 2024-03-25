fd7c2f938c1872ee2e4f20530be13d8057c3c224
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-25 13:10:49 +0900

    [level 1] Title: 폰켓몬, Time: 0.39 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,27 +1,16 @@
-# N마리 폰켓몬 중 N/2 ok
-# 종류에 따라 번호 부여
-# 최대한 다양한 종류의 폰켓몬 가지길 원하기 때문에 많은 종류의 폰켓몬을 포함해 N/2마리 선택
-# 폰켄못 종류번호가 담긴 배열 = nums
-# 폰켓몬 종류 개수의 최댓값 하나만 return
-
-
-# 포켓몬 종류는 여러개고 그 안에 겹치는 애도 있다.
-# 그런데 내가 가지고 갈 수 있는 최대의 개수는 N/2 이다. 
-# 그치만 N/2에는 겹치는 애들이 있을 수 있다. 
-# 그래서 종류가 몇갠지 그것이 중요하다 왜냐 안겹치니까~!
+# 총 N마리의 폰켓몬 중 N/2마리 가져갈 수 있음
+# 폰켓몬은 종류에 따라 번호를 붙여 구분함 
+# 최대한 다양한 종류가 담긴 N/2마리의 폰켓몬을 가져가고자 함
+# N/2마리의 폰켓몬 중 최대한 다른 종류의 폰켓몬의 수를 구하라 
 
 def solution(nums): 
     answer = 0
-    pot = {}
-    for i in nums:
-        pot[i] = pot.get(i,0)+1
-    num_pocket_type = len(pot)
-    max_products = len(nums)//2 
-    
-    if num_pocket_type >= max_products: 
-        answer = max_products
-    else: 
-        answer = num_pocket_type
+    # 폰켓몬의 종류가 몇 갠지 확인한다.
+    type_cnt = len(set(nums))
+    # 가져갈 수 있는 폰켓몬 수를 구한다. 
+    max_cnt = len(nums) // 2 
+    # 폰켓몬 종류는 몇 마리 
+    answer = min(type_cnt, max_cnt)
     return answer 
     
     
