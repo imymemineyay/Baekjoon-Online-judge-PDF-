5088323f01902e814a2838017067512386ea7c59
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-25 19:30:38 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,8 +1,15 @@
-WITH cs_y AS (
-        SELECT A.apnt_ymd, A.apnt_no, A.pt_no, A.mcdp_cd, A.mddr_id, P.pt_name
-            FROM patient AS P JOIN appointment AS A ON P.pt_no = A.pt_no
-            WHERE  A.apnt_ymd LIKE '2022-04-13%' AND A.apnt_cncl_yn = 'N' AND A.mcdp_cd='CS')
-            
-SELECT apnt_no, pt_name, pt_no, cs_y.mcdp_cd, dr_name, apnt_ymd
-    FROM cs_y JOIN doctor ON dr_id = mddr_id
-    ORDER BY apnt_ymd;
+-- #2 
+-- '2022-04-13' 취소되지 않은 흉부외과 
+-- 진료예약번호(appointment), 환자이름(patient), 환자번호(appointment), 
+-- 진료과코드(appointment), 의사이름(doctor), 진료예약일시(appointment)
+
+-- doctor랑 appointment랑 결합한 후 예약일시와 , 흉부외과를 기준으로 데이터 추출
+-- 위 테이블과 patient 결합 
+
+SELECT a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
+FROM appointment AS a 
+    JOIN doctor AS d ON a.mddr_id = d.dr_id
+    JOIN patient AS p USING(pt_no)
+WHERE DATE_FORMAT(a.apnt_ymd,'%Y-%m-%d')='2022-04-13' 
+    AND a.mcdp_cd='CS' AND a.apnt_cncl_yn='N'
+ORDER BY 6;
