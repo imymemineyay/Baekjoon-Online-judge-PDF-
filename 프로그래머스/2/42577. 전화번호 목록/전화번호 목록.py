a5bd88e524f6bb5021762dc01ed495fd20dcf97c
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-25 13:43:41 +0900

    [level 2] Title: 전화번호 목록, Time: 519.95 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,17 +1,18 @@
 def solution(phone_book):
     answer = True
-    pot = {}
-
+    
+    # 전체 번호가 담긴 통을 준비한다. 
+    phone_nums = {}
     for i in phone_book:
-        pot[i] = pot.get(i,1)
-
-    # 번호가 다른 번호로 시작하는지 확인
-    for k in pot:
-        # 숫자 앞자리씩 가져와서 통에 들어있는지 확인 
-        phone = ''
-        for j in k :
-            phone += j
-            if phone in pot and phone != k:
+        phone_nums[i] = phone_nums.get(i,0)+1
+        
+    # 앞서 만든 통 중 번호를 하나씩 뽑아서 변수에 담는다.
+    for phone in phone_nums:
+        num = ''
+    # 해당 번호가 전체 번호에 있는지 확인한다. 단, 자기자신과는 다른 번호여야한다. 
+        for j in phone:
+            num += j
+            if num in phone_nums and num != phone:
                 answer = False
-
+                break
     return answer 
