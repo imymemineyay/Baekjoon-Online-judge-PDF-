82cca3a91ab4dfcceb51bf239d8686acd3988dbf
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-25 19:03:52 +0900

    [level 1] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,10 +1,12 @@
-SELECT HISTORY_ID, 
-       CAR_ID, 
-       DATE_FORMAT(START_DATE,'%Y-%m-%d') START_DATE, 
-       DATE_FORMAT(END_DATE,'%Y-%m-%d') END_DATE,
-       CASE WHEN DATEDIFF(END_DATE,START_DATE)+1>=30  THEN '장기 대여'
-            WHEN DATEDIFF(END_DATE,START_DATE)+1<30 THEN '단기 대여'
-       END RENT_TYPE
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-WHERE START_DATE BETWEEN '2022-09-01' AND '2022-09-30'
-ORDER BY HISTORY_ID DESC;
+-- #2 
+
+SELECT history_id, car_id, 
+       DATE_FORMAT(start_date, '%Y-%m-%d') AS start_date, 
+       DATE_FORMAT(end_date,'%Y-%m-%d') AS end_date, 
+         CASE 
+            WHEN DATEDIFF(end_date,start_date)+1 >= 30 THEN '장기 대여'
+            ELSE '단기 대여'
+         END AS rent_type
+FROM car_rental_company_rental_history 
+WHERE MONTH(start_date) = 9
+ORDER BY history_id DESC;
