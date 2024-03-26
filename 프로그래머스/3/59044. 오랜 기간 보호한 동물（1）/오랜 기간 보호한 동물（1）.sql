5f68f7ca3ecfdf9062c9632bdc82c47f5a7ab71d
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-26 20:22:31 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,7 @@
--- #2 
+-- #3
 
 SELECT i.name, i.datetime
 FROM animal_ins AS i LEFT JOIN animal_outs AS o USING(animal_id)
-WHERE o.datetime IS NULL 
+WHERE o.datetime IS NULL
 ORDER BY i.datetime 
 LIMIT 3;
