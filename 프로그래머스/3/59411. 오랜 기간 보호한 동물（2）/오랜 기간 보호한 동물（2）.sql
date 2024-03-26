7882456163105e1d830943fb7d3b39f69f8fe868
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-27 00:08:59 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,6 +1,6 @@
--- #2
-SELECT i.animal_id, i.name 
-FROM animal_ins AS i LEFT JOIN animal_outs AS o USING(animal_id)
-WHERE o.datetime IS NOT NULL
-ORDER BY DATEDIFF(o.datetime,i.datetime)+1 DESC
+-- #3
+
+SELECT i.animal_id, i.name
+FROM animal_ins AS i JOIN animal_outs AS o USING(animal_id)
+ORDER BY o.datetime - i.datetime DESC
 LIMIT 2;
