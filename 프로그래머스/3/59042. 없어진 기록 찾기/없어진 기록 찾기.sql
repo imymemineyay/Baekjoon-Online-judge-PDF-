bcd14a9651a72bb0e8da89adeb86986defef6b2a
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-27 19:28:23 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,6 @@
--- #2 
+-- #3
 
 SELECT o.animal_id, o.name
-FROM animal_outs AS o LEFT OUTER JOIN animal_ins AS i USING (animal_id)
+FROM  animal_outs AS o LEFT JOIN animal_ins AS i USING(animal_id)
 WHERE i.datetime IS NULL
 ORDER BY o.animal_id;
