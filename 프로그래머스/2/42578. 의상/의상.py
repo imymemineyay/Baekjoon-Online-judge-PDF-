23f5982943a7c621e5281b9f3ac3d40bc3addb2e
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-29 05:30:39 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,16 @@
 def solution(clothes):
     answer = 1
-    
-    # 종류별로 옷이 몇 개 있는지 알 수 있는 통을 만든다. 
-    #단, 옷의 개수에 +1을 해줘서 없는 것 까지 고려한다. 
-    clothing = {}
-    for idx in clothes:
-        kinds = idx[1]
-        clothing[kinds] = clothing.get(kinds,1) +1     
-    # 종류별 옷의 개수를 곱해준 후 모든 옷을 안입었을 경우의 경우의 수를 빼준다. 
-    for type in clothing:
-        answer *= clothing[type]
-    answer -= 1
+    # clothes의 요소를 일일이 추출하며
+    item_num = dict()
+    for item in clothes:
+    #     딕셔너리에 종류별로 몇개의 아이템이 있는지 담는다.
+        item_num[item[1]] = item_num.get(item[1],0) + 1
+
+    # 딕셔너리에 있는 요소를 일일이 추출하며 
+    for ele in item_num:
+    #     각 종유별로 몇 개의 아이템이 있는지 구한다. 아이템의 개수 +1한 수를 answer에 곱해준다. 
+        answer *= item_num[ele] +1 
+
+    # 마지막에 answer -1 을 해준다.
+    answer -= 1 
     return answer
