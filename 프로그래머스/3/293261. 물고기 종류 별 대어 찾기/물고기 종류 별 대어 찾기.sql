7f1378c5de6beb89f44da5a12329ca35ef6a31d2
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-29 00:08:42 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,9 +1,8 @@
-WITH max_length_fish AS (SELECT * 
-                         FROM fish_name_info 
-                            JOIN (SELECT fish_type, MAX(length) AS length
-                                    FROM fish_info 
-                                    GROUP BY fish_type) AS b 
-                            USING(fish_type))
+-- #2
 
 SELECT id, fish_name, length
-FROM fish_info JOIN max_length_fish USING(fish_type, length)
+FROM fish_info JOIN fish_name_info USING(fish_type)
+WHERE (fish_type, length) IN (SELECT fish_type, MAX(length) AS length
+                            FROM fish_info
+                            GROUP BY fish_type)
+ORDER BY id;
