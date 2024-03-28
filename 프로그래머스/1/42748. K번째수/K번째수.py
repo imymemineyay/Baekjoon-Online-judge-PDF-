adebd611e559756358d0b2a3fe7006a92d8aa18e
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-29 05:40:39 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,10 @@
 def solution(array, commands):
     answer = []
-    for i,j,k in commands:
-        answer.append(sorted(array[i-1:j])[k-1])
-    
-    return answer
+    # commands의 명령안 요소를 하나씩 추출한다.
+    for a,b,c in commands:
+    #     하나의 요소 안에 있는 명령을 실행한다. 
+    #     배열의 i-1부터 n까지 자른 후, 정렬시킨다.
+        lst = sorted(array[a-1:b])
+    #      정렬된 배열 중 m-1번째 값을 answer에 담는다.
+        answer.append(lst[c-1])
+    return [(sorted(array[i-1:j])[k-1]) for i,j,k in commands]
