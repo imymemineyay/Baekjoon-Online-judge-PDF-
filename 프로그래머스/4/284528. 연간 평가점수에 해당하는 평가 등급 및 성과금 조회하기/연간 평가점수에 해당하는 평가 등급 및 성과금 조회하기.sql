6b7b9d5c9b77d7d7df3c87aa70c6af23759b9f6d
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-28 23:27:29 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,18 +1,19 @@
--- #2
+-- #3
 
 SELECT emp_no, emp_name, 
-        CASE 
-            WHEN AVG(score)>=96 THEN 'S'
-            WHEN AVG(score)>=90 THEN 'A'
-            WHEN AVG(score)>=80 THEN 'B'
-            ELSE 'C'
-        END AS grade,
-        CASE 
-            WHEN AVG(score)>=96 THEN sal*0.2
-            WHEN AVG(score)>=90 THEN sal*0.15
-            WHEN AVG(score)>=80 THEN sal*0.1
-            ELSE 0
-        END AS bonus
-FROM hr_employees JOIN hr_grade USING (emp_no)
+    CASE 
+        WHEN AVG(score) >= 96 THEN 'S'
+        WHEN AVG(score) >= 90 THEN 'A'
+        WHEN AVG(score) >= 80 THEN 'B'
+        ELSE 'C' 
+    END AS grade, 
+    CASE 
+        WHEN AVG(score) >= 96 THEN sal * 0.2
+        WHEN AVG(score) >= 90 THEN sal * 0.15
+        WHEN AVG(score) >= 80 THEN sal * 0.1
+        ELSE sal * 0
+    END AS bonus
+FROM hr_employees JOIN hr_department USING(dept_id)
+    JOIN hr_grade USING(emp_no)
 GROUP BY emp_no
 ORDER BY emp_no;
