a9567a2c1a29d7f4d97dbdbcfc9b2e3022a63444
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-29 06:08:17 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,34 +1,12 @@
 def solution(numbers):
-    # 1. 조건이 1000이하이니 모든 백단위로 맞춰준 숫자와 새로 만든 수가 담긴 통을 만든다.
-    number_ranking = {}
     answer = ''
-    for num in numbers:
-        if num < 10:
-            number_ranking[num] = number_ranking.get(num, num * 100)
-        elif num < 100:
-            number_ranking[num] = number_ranking.get(num, num * 10)
-        else:
-            number_ranking[num] = number_ranking.get(num, num)
-
-    # 2. 통을 새로 만든 수를 기준으로 정렬해준다.
-    result_ranking = sorted(number_ranking, key=lambda x: number_ranking[x], reverse=True)
-
-    # 3. 리스트 안의 수를 출력한다.
-    for i in result_ranking:
-        if answer == '':
-            answer = '0' if i == 0 else str(i)
-        else:
-            answer += str(i)
-
-    return answer
-def solution(numbers):
-    # 주어진 숫자들을 문자열로 변환합니다.
-    numbers = list(map(str, numbers))
     
-    # 숫자를 두 번 이어붙여 비교하여 더 큰 순서로 정렬합니다.
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    
-    # 리스트의 모든 요소를 이어붙여 문자열로 반환합니다.
-    answer = str(int(''.join(numbers)))
-    
+    i = len(numbers)
+    # 1. numbers의 요소를 문자 타입으로 변경한다.  
+    numbers = list(map(str,numbers))
+    # 2. 똑같은 수를 (i-1)만큼 반복한 수를 기준으로 numbers를 정렬한다. 
+    sorted_numbers = sorted(numbers, key = lambda x : x*3, reverse=True)
+    # 3. 정렬된 배열을 정수로 바꿔준 후 문자열로 변경한다. 
+    answer = str(int(''.join(sorted_numbers)))
+
     return answer
