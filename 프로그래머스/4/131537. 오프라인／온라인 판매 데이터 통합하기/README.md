e427e87c629c7a52500668a264e0aadb5b09574f
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-28 20:54:32 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/README.md
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 오프라인/온라인 판매 데이터 통합하기 - 131537 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131537#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131537) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 03월 10일 19:05:50
+2024년 03월 28일 20:54:25
 
 ### 문제 설명
 
--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,12 +1,12 @@
--- # 2
+-- #3
 
-SELECT DATE_FORMAT(sales_date,'%Y-%m-%d') AS sales_date, product_id, 
-        user_id, sales_amount 
-    FROM online_sale 
-    WHERE sales_date like '2022-03%' 
-UNION
-SELECT DATE_FORMAT(sales_date,'%Y-%m-%d') AS sales_date, product_id, 
-        NULL AS user_id, sales_amount
-    FROM offline_sale
-    WHERE sales_date like '2022-03%' 
+SELECT DATE_FORMAT(sales_date,'%Y-%m-%d') AS sales_date, 
+        product_id, user_id, sales_amount 
+FROM online_sale
+WHERE DATE_FORMAT(sales_date,'%Y-%m') = '2022-03'
+UNION 
+SELECT DATE_FORMAT(sales_date,'%Y-%m-%d') AS sales_date, 
+    product_id,  NULL AS user_id, sales_amount 
+FROM offline_sale
+WHERE DATE_FORMAT(sales_date,'%Y-%m') = '2022-03'
 ORDER BY sales_date, product_id, user_id;
