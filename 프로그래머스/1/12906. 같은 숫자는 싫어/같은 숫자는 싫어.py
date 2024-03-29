601608c658104dd8ea780f086c17541700916e6f
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-30 01:04:14 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 158.08 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,18 +1,22 @@
+from collections import deque
+
+
 def solution(arr):
     answer = []
-    arr_length_minus_1 = len(arr)-1
-    # - 앞에서부터 arr의 길이 -1한 숫자만큼 뒷수와 비교한다. 
-    for i in range(arr_length_minus_1):  
-        # 만약 본인과 뒷수가 같지않다면, 본인의 숫자를 answer에 담는다 
+    arr.append(arr[0]+1)
 
-        if arr[i] != arr[i+1]:
-            answer.append(arr[i])
-            # 단, 본인의 수가 마지막 인덱스라면 뒷자리 숫자 역시 answer에 담아준다 
-            if i == arr_length_minus_1-1:
-                answer.append(arr[i+1])
-            #  그렇지 않다면 마지막 인덱스의 경우에만 answer값에 담아준다. <br>
-        if (i == arr_length_minus_1-1)&(arr[i] == arr[i+1]): 
-            answer.append(arr[i])
+    arr_dequeue = deque(arr)
+
+    for number in list(arr_dequeue):
+        if len(arr_dequeue) <= 1:
+            break
+
+        else:
+            if number != arr_dequeue[1]:
+                num = arr_dequeue.popleft()
+                answer.append(num)
+            else:
+                arr_dequeue.popleft()
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     print('Hello Python')
     return answer
