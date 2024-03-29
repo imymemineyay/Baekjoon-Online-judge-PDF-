5ccb82eef543bcdae270df255880f5864d0c4d0f
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-29 16:41:16 +0900

    [level 1] Title: 모의고사, Time: 6.82 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,35 +1,25 @@
 def solution(answers):
     answer = []
-    cnt = len(answers)	
+    answers = list(map(str,answers))
 
-    answer = []
-    # 1. 1~3번 수포자의 정답지를 만든다.
-    suhak_hater=[]
-    suhak_hater.append(['12345'*2000][0][:cnt]) # 1번 
-    suhak_hater.append(['21232425'*(10000//8)][0][:cnt]) # 2번
-    suhak_hater.append(['3311224455' * (1000)][0][:cnt]) # 3번
+    number_1 = '12345' * 2000
+    number_2 = '21232425' * (10000//8)
+    number_3 = '3311224455' * 1000
 
-    # 2. 답안지를 문자형으로 변환시켜준다.
-    answers = ''.join(list(map(str,answers)))
+    correct_cnt = [0,0,0]
+
+    for idx, num in enumerate(answers):
+        if num == number_1[idx]:
+            correct_cnt[0] += 1
+        if num == number_2[idx]:
+            correct_cnt[1] += 1
+        if num == number_3[idx]:
+            correct_cnt[2] += 1
 
 
-    # 2. 수포자의 정답지와 실제 정답지를 일일히 비교하여 정답을 맞힌 갯수를 통에 담는다.
-    correct_num = []
-    for name, pick_num in enumerate(suhak_hater):
-        correct_cnt = 0
-        for idx, num in enumerate(answers):
-            if num == pick_num[idx]:
-                correct_cnt += 1
-            else: correct_cnt += 0
-        correct_num.append([name,correct_cnt])   
+    max_score = max(correct_cnt)
 
-    # 3. 갯수를 기준으로 정렬하고, 만약 같은 값이 여러개라면 이름을 기준으로 오름차순 해준다.
-    correct_num = sorted(correct_num,key=lambda x: (-x[1], x[0]))
-
-    if correct_num[0][1] ==  correct_num[2][1]:
-        answer = [correct_num[i][0]+1 for i in range(3)]
-    elif correct_num[0][1] == correct_num[1][1] : 
-        answer = [correct_num[i][0]+1 for i in range(2)]
-    else:
-        answer = [correct_num[0][0]+1]
+    for idx, score in enumerate(correct_cnt):
+        if score == max_score:
+            answer.append(idx+1)
     return answer
