b5259c546b7b3bb99e0ef9776b56821f660b22eb
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-03-29 16:59:18 +0900

    [level 2] Title: 소수 찾기, Time: 0.63 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -2,27 +2,26 @@ from itertools import permutations
 
 
 # 순열 확인 함수
-def is_prime_number(number):
-    if number == 0 or number == 1: # 0,1 은 소수가 아니다
+def is_prime_number(num):
+    if num == 0 or num == 1:
         return False
     
-    for i in range(2,number): # 1과 자기자신을 제외한 숫자들로 나누어 떨어지면 합성수다
-        if not (number % i):
+    for i in range(2, num):
+        if num % i == 0:
             return False
-        
-    return True # 나누어 떨어지지 않으면 소수다. 
+    return True
 
 
 
 def solution(numbers):
     answer = 0
-    # 1. numbers에 있는 수들로 모든 자리수별로 만들 수 있는 모든 수들을 만든다. 
-    johabs_set = set()
-    for n in range(1,len(numbers)+1) :
-        # 단, 같은 수가 있는 경우가 있으니 중복을 제거해줘야한다. 
-        johabs_set.update(set(map(lambda x : int(''.join(x)), permutations(numbers,n))))
+    num_set = set()
+    for i in range(1, len(numbers)+1):
+        shu = list(map(lambda x: int(''.join(x)),list(permutations(numbers, i))))
+        num_set.update(shu)
 
-    for johab in johabs_set:
-        if is_prime_number(johab):
+    for n in num_set:
+        if is_prime_number(n):
             answer += 1
+
     return answer
