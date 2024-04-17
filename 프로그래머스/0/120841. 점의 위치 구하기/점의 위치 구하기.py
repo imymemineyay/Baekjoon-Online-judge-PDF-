c5214dc87759be7d87fd6cf9bdf912f0b2056d94
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-04-17 10:11:59 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,12 @@
+def solution(dot):
+    answer = 0
+    x,y = dot
+    if x > 0 and y > 0:
+        answer = 1
+    elif x < 0 and y > 0:
+        answer = 2
+    elif x < 0 and y < 0:
+        answer = 3
+    elif x > 0 and y < 0:
+        answer = 4
+    return answer
