e4f2d6979742d2c388b1c8458ffaae3178f9b1e8
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-04-24 12:31:04 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i.islower():
+            answer += i.upper()
+        else:
+            answer += i.lower()
+    return answer
