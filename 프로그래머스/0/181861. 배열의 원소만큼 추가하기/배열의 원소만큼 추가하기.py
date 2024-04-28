87461e5b27e6f79d990eb421700aad6caa7b28ec
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-04-28 12:28:55 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        for j in range(arr[i]):
+            answer.append(arr[i])
+    return answer
+            
+            
+     
