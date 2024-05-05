d9eda5c90516c4f087128b765b691ad75ce34002
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-05-05 12:56:52 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+    answer = 1
+    if len(num_list) > 10:
+        answer = sum(num_list)
+    elif len(num_list) <= 10:
+        for i in num_list:
+            answer *= i
+    return answer
