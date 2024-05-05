e27d6f01aec453654ecf28648dce06c8e963e37a
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-05-05 13:02:53 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 12.47 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    for i in arr:
+        if i >= 50 and i % 2 ==0:
+            answer.append(i//2)
+        elif i < 50 and i % 2 != 0:
+            answer.append(i*2)
+        else:
+            answer.append(i)
+    
+    return answer
