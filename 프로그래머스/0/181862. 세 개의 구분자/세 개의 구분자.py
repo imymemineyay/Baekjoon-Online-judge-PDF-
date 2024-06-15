ff3ec10c77b7cf35e8b556d66249744756b20e46
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-06-15 14:03:52 +0900

    [level 0] Title: 세 개의 구분자, Time: 7.13 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,21 +1,5 @@
 def solution(myStr):
-    ans = []
-    lst = myStr.split('a')
-    for i in lst:
-        ans.extend(i.split('b'))
-        
-    answer = []
-    for i in ans:
-        answer.extend(i.split('c'))
+    result = myStr.replace('a',' ').replace('b',' ').replace('c',' ').split()
     
-    result = []
-    for i in answer:
-        if i != '':
-            result.append(i)
-    
-    
-    if len(result) == 0:
-        result.append('EMPTY')
-        
-    return result
+    return result if result else ['EMPTY']
             
