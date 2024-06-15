0508f81233a6b1ef01e0e56338607c10b46a70f8
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-06-15 13:40:46 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -0,0 +1,7 @@
+def solution(myString, pat):    
+    answer = 0
+    for i in range(len(myString)-(len(pat)-1)):
+        if myString[i:i+len(pat)] == pat:
+            print(myString[i:i+len(pat)])
+            answer += 1 
+    return answer 
