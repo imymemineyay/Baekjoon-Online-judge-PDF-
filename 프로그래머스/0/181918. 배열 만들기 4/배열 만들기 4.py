71fb082a2183f08a184993317b648d2de8d4d10d
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-07-01 15:06:25 +0900

    [level 0] Title: 배열 만들기 4, Time: 7.09 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    stk = []
+    i = 0 
+    while i < len(arr) : 
+        if len(stk) == 0:
+            stk.append(arr[i]) 
+            i += 1 
+        elif len(stk) > 0:
+            if stk[-1] < arr[i]:
+                stk.append(arr[i])
+                i += 1 
+            elif stk[-1] >= arr[i]:
+                stk = stk[:-1]
+    return stk
