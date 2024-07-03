50e9529d31d664def670216d26cceeafb167ebf3
Choi, Jungin <117002193+imymemineyay@users.noreply.github.com>
2024-07-03 15:12:04 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,2 +1,14 @@
 def solution(my_string):
-    return eval(my_string)
+    # "3 + 4 + 5 + 1 - 2"
+    # +는 0으로 바꾸고 - 는 뒤의 숫자의 2배를 마이너스한 값 담기 
+    lst = []
+    my_string = my_string.split(' ')
+    for idx, string in enumerate(my_string):
+        if string == '+':
+            lst.append(0)
+        elif string == '-':
+            lst.append(int(my_string[idx+1])*-2)
+        else :
+            lst.append(string)
+        
+    return sum(list(map(int, lst)))
